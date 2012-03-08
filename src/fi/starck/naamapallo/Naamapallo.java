33854836d111b96179fcd912637a1e5d50036ed5
Tuomas Starck <dev@starck.fi>
2012-03-08 21:36:53 +0200

    Naamapallo alkaa, nostakaamme jalkaa.

---
+++ src/fi/starck/naamapallo/Naamapallo.java
@@ -0,0 +1,12 @@
+package fi.starck.naamapallo;
+
+/**
+ * @author Tuomas Starck
+ */
+public class Naamapallo {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+    }
+}
